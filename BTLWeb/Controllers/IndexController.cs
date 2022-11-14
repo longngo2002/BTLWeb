71866ff4da0163eef808708f89583e8d569e9c28
longngo2002 <52597487+longngo2002@users.noreply.github.com>
2022-11-14 09:37:46 +0700

    first commit

---
+++ BTLWeb/Controllers/IndexController.cs
@@ -0,0 +1,27 @@
+﻿using BTLWeb.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTLWeb.Controllers
+{
+    public class IndexController : Controller
+    {
+        banlaptopEntities db = new banlaptopEntities();
+        // GET: Index
+        public ActionResult Index(int? page)
+        {
+            List<category> lstcate;
+
+            int pagesize = 16; //so sp toi da tren 1 trang 
+            int pagenumber = (page ?? 1); //neu null thi nhan 1
+
+            return View();
+        }
+
+
+    }
+}    
---
+++ BTLWeb/Controllers/productsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BTLWeb.Models;
+
+namespace BTLWeb.Controllers
+{
+    public class productsController : Controller
+    {
+        private banlaptopEntities db = new banlaptopEntities();
+
+        // GET: products
+        public ActionResult Index()
+        {
+            var products = db.products.Include(p => p.category);
+            return View(products.ToList());
+        }
+
+        // GET: products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: products/Create
+        public ActionResult Create()
+        {
+            ViewBag.category_id = new SelectList(db.categories, "id", "name");
+            return View();
+        }
+
+        // POST: products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "name,updated_date,status,deleted,price,price_sale,id,quantity,description,isHot,create_at,rate,avatar_url,category_id,create_user")] product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.category_id = new SelectList(db.categories, "id", "name", product.category_id);
+            return View(product);
+        }
+
+        // GET: products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.category_id = new SelectList(db.categories, "id", "name", product.category_id);
+            return View(product);
+        }
+
+        // POST: products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "name,updated_date,status,deleted,price,price_sale,id,quantity,description,isHot,create_at,rate,avatar_url,category_id,create_user")] product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.category_id = new SelectList(db.categories, "id", "name", product.category_id);
+            return View(product);
+        }
+
+        // GET: products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            product product = db.products.Find(id);
+            db.products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
